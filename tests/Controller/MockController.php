12fe5a62aa089567ce8cfbc84a3e50b347a74524
Gievoi Viktor <viktor.gievoi@pimpay.ru>
2021-02-12 13:47:26 +0300

    New tests for loggers

--- tests/Controller/MockController.php
+++ tests/Controller/MockController.php
@@ -2,12 +2,22 @@
 
 namespace Vasary\TracingBundle\Tests\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 final class MockController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function index(): Response
     {
+        $this->logger->info('Hello PHPUnit');
+
         return new Response();
     }
 }
--- tests/Unit/Controller/ControllerTest.php
+++ tests/Unit/Controller/ControllerTest.php
@@ -9,16 +9,40 @@ class ControllerTest extends WebTestCase
     /**
      * @test
      */
-    public function default(): void
+    public function requestHeaders(): void
     {
         $client = static::createClient(['environment' => 'test']);
-
         $client->request('GET', '/');
 
         $response = $client->getResponse();
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertTrue($response->headers->has('x-trace-id'));
+    }
+
+    /**
+     * @test
+     */
+    public function loggerRecords(): void
+    {
+        $client = static::createClient(['environment' => 'test']);
+        $logger = $client->getKernel()->getContainer()->get('handler.test');
+
+        $client->request('GET', '/');
+
+        self::assertTrue($logger->hasInfoThatMatches('/Hello PHPUnit/'));
+        self::assertTrue($logger->hasInfoThatMatches('/^Entrypoint:\s\[.*\]\s.+/'));
+        self::assertTrue($logger->hasInfoThatMatches('/^Request:\s.*/'));
+        self::assertTrue($logger->hasInfoThatMatches('/^Outgoing response:\s.*/'));
+
+        self::assertGreaterThan(0, count($logger->getRecords()));
+        self::assertArrayHasKey('extra', $logger->getRecords()[0]);
+        self::assertArrayHasKey('key', $logger->getRecords()[0]['extra']);
+        self::assertArrayHasKey('application', $logger->getRecords()[0]['extra']);
+        self::assertArrayHasKey('x-trace-id', $logger->getRecords()[0]['extra']);
+
+        self::assertEquals('change_me', $logger->getRecords()[0]['extra']['application']);
+        self::assertEquals('value', $logger->getRecords()[0]['extra']['key']);
     }
 
     /**
